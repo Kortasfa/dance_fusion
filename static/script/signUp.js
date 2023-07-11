a5bf23ae241c01433b6f031f2057ac86902529a8
daria.ohotnikova <d_okhotnikova@mail.ru>
2023-07-11 10:48:24 -0200

    страница авторизации

--- static/script/signUp.js
+++ static/script/signUp.js
@@ -1,6 +1,11 @@
+const passwordEye = document.querySelector(".field__eye");
+const nameField = document.getElementById("name");
+const passwordField = document.getElementById("password");
+const signUpField = document.querySelector(".entrance");
+const signUpBtn = document.getElementById("btn-sign-up");
+const warningMessage = document.querySelector(".field__warning")
+const messageToLogIn = document.querySelector(".message-log-in")
 function signUp() {
-    let nameField = document.querySelector(".name-field");
-    let passwordField = document.querySelector(".password-field");
     let userInfo = {
         "userName": nameField.value,
         "password": passwordField.value
@@ -10,12 +15,41 @@ function signUp() {
     XHR.open("POST", "/api/sign_up");
     XHR.onload = function () {
         if (XHR.status === 200) {
+            signUpField.classList.add("hidden");
+            messageToLogIn.classList.remove("hidden");
             console.log("Successfully registered!");
         } else if (XHR.status === 409) {
+            nameField.classList.add("warning-border");
+            warningMessage.classList.remove("hidden")
             console.log("Username is taken!");
         } else {
-            console.log("Failed to register!");
+            alert("Failed to register!");
         }
-    };
+    }
     XHR.send(messageContent);
 }
+
+function changeEye() {
+    console.log(passwordField.type);
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        passwordEye.src = "/static/img/eye.svg";
+    }
+    else if (passwordField.type === "text") {
+        passwordField.type = "password";
+        passwordEye.src = "/static/img/eye_off.svg";
+    }
+}
+
+function eye() {
+    if (passwordField.value === "") {
+        passwordEye.classList.add("hidden")
+    }
+    else {
+        passwordEye.classList.remove("hidden")
+    }
+}
+
+passwordField.addEventListener("input", eye);
+passwordEye.addEventListener("click", changeEye);
+signUpBtn.addEventListener("click", signUp)
