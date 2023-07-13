56b88207f3ac6bbcc4efb2ac53db740e61711e93
Kaldyn2004 <kirillkropotov2016@gmail.com>
2023-07-13 10:06:06 +0300

    mvp-0_Kirill 1	Сохранение подключившихся пользователей в cookie, добавление этих пользователей  при загрузке страницы              2	Невозможность запустить игру без игроков и без выбора песни              3	Переделать инструкцию: сократить, перевести на английский              4	Убрать стрелочку назад на начальной странице              5	При нажатии на кнопку выбора песни загорается кнопка выбора песни              6	Кнопка PLAY загорается только при подключении пользователей и выборе песни              7	Испраить названия переменных              8	Убрать линию

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -55,5 +55,5 @@ func main() {
 }
 
 func openDB() (*sql.DB, error) {
-	return sql.Open(dbDriverName, "root:P@ssw0rd@tcp(localhost:3306)/dance_fusion?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=true")
+	return sql.Open(dbDriverName, "root:root@tcp(localhost:3306)/dance_fusion?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=true")
 }
