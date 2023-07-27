5d91d35090b661432d0aa14cbb4750a619b42d81
maksim.nazarov <Apppp5778@gmail.com>
2023-07-27 16:44:57 -0200

    idea about bots

---
+++ data/sql/create bots.sql
@@ -0,0 +1,17 @@
+USE dance_fusion;
+CREATE TABLE bots(
+                     `id` INTEGER AUTO_INCREMENT PRIMARY KEY,
+                     `name` VARCHAR(255) NOT NULL,
+                     `bot_scores_path` VARCHAR(255) NOT NULL,
+                     `img_hat` VARCHAR(255) NOT NULL,
+                     `img_body` VARCHAR(255) NOT NULL,
+                     `img_face` VARCHAR(255) NOT NULL
+) ENGINE = InnoDB
+CHARACTER SET = utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+INSERT INTO bots(name, bot_scores_path, img_hat, img_body, img_face)
+VALUES
+    ("bot_1", "static/bots_list/bot_1.json", "static/img/hat/robinHoodHat.png", "static/img/body/fff76e.png", "static/img/face/7.png"),
+    ("bot_2", "static/bots_list/bot_2.json", "static/img/hat/robinHoodHat.png", "static/img/body/fff76e.png", "static/img/face/7.png");
+SELECT * FROM bots;
