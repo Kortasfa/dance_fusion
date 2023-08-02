5621c027888779053eafa75851fdb1652aca590d
maksim.nazarov <Apppp5778@gmail.com>
2023-08-02 15:36:21 -0200

    fixed bots and motions in dances ForgetYou and Rasputin

--- data/sql/create bots.sql
+++ data/sql/create bots.sql
@@ -11,10 +11,18 @@ CREATE TABLE bots(
 CHARACTER SET = utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
-INSERT INTO bots(bot_id, bot_name, bot_scores_path, img_hat, img_body, img_face, difficulty)
+INSERT INTO bots(bot_name, bot_scores_path, img_hat, img_body, img_face, difficulty)
 VALUES
-(1, 'Maxim', 'static/bots_list/bot_1.json', 'static/img/hat/robinHoodHat.png', 'static/img/body/fff76e.png', 'static/img/face/7.png', 4),
-(2, 'Kirill', 'static/bots_list/bot_2.json', 'static/img/hat/bigHat.png', 'static/img/body/greenBody.png', '../static/img/face/pickUpFace.png', 3),
-(3, 'Zaxar', 'static/bots_list/bot_2.json', 'static/img/hat/cowboyHat.png', 'static/img/body/89e7dc.png', '../static/img/face/1.png', 2),
-(4, 'Valeryan', 'static/bots_list/bot_2.json', 'static/img/hat/stanHat.png', 'static/img/body/50a7ea.png', '../static/img/face/badFace.png', 1),
-(5, 'Dasha', 'static/bots_list/bot_2.json', 'static/img/hat/herHat.png', 'static/img/body/fe5955.png', '../static/img/face/angriestFace.png', 1);
+    ('Maxim', 'static/bots_list/bot_charged_maksim.json', 'static/img/hat/withoutHat.png', 'static/img/special/ronaldDrink.gif', 'static/img/face/withoutFace.png', 3),
+    ('Kirill', 'static/bots_list/bot_fury_kirill.json', 'static/img/hat/cilindrHat.png', 'static/img/body/greenBody.png', '../static/img/face/loveFace.png', 3),
+    ('Zaxar', 'static/bots_list/bot_smart_zaxar.json', 'static/img/hat/withoutHat.png', 'static/img/special/papichBarMontage.gif', '../static/img/face/withoutFace.png', 2),
+    ('Valeryan', 'static/bots_list/bot_up_down_hand_valeryan.json', 'static/img/hat/withoutHat.png', 'static/img/special/siBody.png', '../static/img/face/withoutFace.png', 1),
+    ('Dasha', 'static/bots_list/bot_super_dasha.json', 'static/img/hat/robinHoodHat.png', 'static/img/body/fe73ac.png', '../static/img/face/badFace.png', 1),
+    ('Michael Jackson', 'static/bots_list/bot_michael_jackson.json', 'static/img/hat/withoutHat.png', 'static/img/special/michaelJackson.png', '../static/img/face/withoutFace.png', 4),
+    ('Kerim', 'static/bots_list/bot_kerim.json', 'static/img/hat/beretHat.png', 'static/img/body/fe5955.png', '../static/img/face/pickUpFace.png', 4);
+select * from bots;
+
+
+
+
+
