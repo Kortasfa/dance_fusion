b181de141f446fc0ebf02c45d393fac99afbb080
Kaldyn2004 <kirillkropotov2016@gmail.com>
2023-08-04 12:37:48 +0300

    mvp-0_Kirill boss bar, play with-out user not available

--- static/script/gameField.js
+++ static/script/gameField.js
@@ -14,7 +14,7 @@ const starThreeScale = document.getElementById("star-3");
 const starFourScale = document.getElementById("star-4");
 const starFiveScale = document.getElementById("star-5");
 const megaStarScale = document.getElementById("mega-star");
-const bossHpBar = document.querySelector('.boss__hp-bar');
+const bossHpBar = document.querySelector('.hp-bar');
 
 function getUsersByCookie() {
     for (let i = 0; i < connectedUsers.length; i++) {
@@ -190,7 +190,8 @@ function addScore(userID, score, maxScore) {
 let hpBar;
 function playerDamage(score) {
     bossHp = bossHp - score;
-    hpBar =  100 * (bossHp / bossHealth);
+    hpBar =  190 * (bossHp / bossHealth);
+
     bossHpBar.style.width = hpBar + 'px';
 }
 
@@ -213,7 +214,6 @@ function expelUsers() {
 if (mode == 'Boss') {
     document.querySelector(".boss-container").classList.remove("hidden");
     document.querySelector(".boss__name").innerText = bossInfo.name;
-    // document.querySelector(".boss__hp-bar").innerText = (parseInt(bossInfo.healthPoint) * connectedUsers.length).toString();
     document.querySelector(".boss__body-img").src = bossInfo.bossBody;
     document.querySelector(".boss__face-img").src = bossInfo.bossFace;
     document.querySelector(".boss__hat-img").src = bossInfo.bossHat;
